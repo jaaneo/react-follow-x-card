8cddc329230fae33cf5fa34f43728a0750b2afed
Alejandro Sáez <jaaneo@gmail.com>
2024-03-05 16:44:30 -0300

    tic tac toe

---
+++ projects/02-gato/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+
+const Square = ({ children, updateBoard, index }) => {
+  return (
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'])
+
+  return (
+    <main className='board'>
+      <h1>Tic tac toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return  (
+              <Square
+              key={index}
+              index = {index}>
+                  {board[index]}
+              </Square>
+            )
+          })}
+      </section>
+    </main>
+  )
+}
+
+export default App
